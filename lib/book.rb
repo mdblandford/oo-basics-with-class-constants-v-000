444d1344a06c1554100bf5ad97b965b81321400e
Michael Blandford <mdblandford@gmail.com>
2018-06-24 16:16:55 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,15 +3,16 @@ class Book
   attr_reader :title, :genre
 
   GENRES = []
-  
+
   def initialize(title)
     @title = title
+    GENRES << genre
   end
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  def genre= (genre)
-      @genre = genre
-      GENRES << genre
-  end
+#  def genre= (genre)
+#      @genre = genre
+#      GENRES << genre
+#  end
 end
